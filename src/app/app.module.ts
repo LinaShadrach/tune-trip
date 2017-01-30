43d5b1339ef3a46a496e549abbf28e08c7d5d30c
Alex Francois and Eric Raetz <alex@>
2017-01-30 10:57:28 -0800

    add functioning sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
-import { masterFirebaseConfig } from './api-keys';
 
 import { AppComponent } from './app.component';
 import { SplashComponent } from './splash/splash.component';
 import { ShowListComponent } from './show-list/show-list.component';
-
-export const firebaseConfig = {
-  apiKey: masterFirebaseConfig.apiKey,
-  authDomain: masterFirebaseConfig.authDomain,
-  databaseURL: masterFirebaseConfig.databaseURL,
-  storageBucket: masterFirebaseConfig.storageBucket
-};
 
 @NgModule({
   declarations: [
