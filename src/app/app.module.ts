2f0a2c10af045abd03ba6a33dfe77b327dba4a53
Alex Francois and Eric Raetz <alex@>
2017-01-31 13:28:47 -0800

    solve zindex problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
-
 import { AppComponent } from './app.component';
 import { SplashComponent } from './splash/splash.component';
 import { ShowListComponent } from './show-list/show-list.component';
