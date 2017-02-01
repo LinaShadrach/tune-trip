79c714edbc05a075dc5f03c2756123f7340e4e79
Kevin Finley and Lina Shadrach <kevin+lina@>
2017-02-01 13:36:00 -0800

    add logic for searching when user signs in

--- src/app/show-list/show-list.component.ts
+++ src/app/show-list/show-list.component.ts
@@ -34,8 +34,45 @@ export class ShowListComponent implements OnInit {
     this.userToDisplay = this.userService.getUserById(this.userId);
     this.currentUsername=this.userService.setUsername(this.userId);
     this.artistList=[];
+    this.searchOnInit();
   }
 
+  searchOnInit(){
+    this.showResults=true;
+    console.log(this.artistList.length);
+    this.topTracks = this.lastFMService.getTopTracks(this.currentUsername).subscribe(data=>{
+      for(var i=0; i<data.json().toptracks.track.length; i++){
+        this.lastFMService.getSimilarTracks(data.json().toptracks.track[i]).subscribe(response=>{
+          this.getArtistsInit(response);
+          if(i=data.json().toptracks.track.length-1){
+            this.done=true;
+          }
+        });
+      };
+    });
+  }
+  getArtistsInit(response){
+    var currentTrack;
+    for(var i=0; i<response.json().similartracks.track.length; i++){
+      if(this.artistList!==undefined){
+          currentTrack=this.songKickService.getArtists(response.json().similartracks.track[i]).subscribe(result=>{
+            if(result.json().resultsPage.status!=="error"){
+              this.artistList.push(result.json());
+              if(result.json().resultsPage.results.event){
+                console.log(result.json());
+              }
+            }
+        });
+      }
+      else {
+        currentTrack=this.songKickService.getArtists(response.json().similarartiistLst.track[i]).subscribe(result=>{
+          if(result.json().resultsPage.status!=="error"){
+            this.artistList=[result.json().resultsPage.results.totalEntries];
+          }
+        });
+      }
+    }
+  }
   search(){
     this.showResults=true;
     console.log(this.artistList.length);
